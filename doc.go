a2ca4820b3968ad0272f9c210f2ca8ead68f0258
ian ferguson <ian@labmarie.com>
2015-02-07 13:43:06 -0500

    Add initial godoc

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package cryptopals contains a series of tests that are the solutions for various
+// challenges in the Matasano Cryptopals challenges (http://cryptopals.com/)
+// The solution tests all utilize tools found in various subpackage of this package
+package cryptopals
